4a6ad12176ecc3c601fd3188362aec5f00c8db8f
jaya-shankar <bjayashankar2102@gmail.com>
2024-10-27 11:33:22 +0530

    add web folder

---
+++ web/scripts/KPI_extract.py
@@ -0,0 +1,195 @@
+
+
+
+'''
+column names = ['timestamp', 'email_address', 'project_url', 'project_name', 'checklist_number', 'year_of_funding', 'project_partner_email', "project_partner's_website", 'number_of_years_with_asha', 'name_of_person_filling_in_this_form', 'chapter_affiliation', 'affiliation_of_person_filling_the_form', 'percentage_of_budget_supported_by_asha', 'percentage_of_project_partner_operating_expense_supported_by_asha', 'does_the_project_partner_charge_fees_for_their_services?_if_so__what_is_the_average_monthly_charge_per_beneficiary._(please_enter_0_if_no_charges_being_levied)', 'working_capital_ratio_of_project_partner', 'which_of_the_following_sources_of_funding_are_applicable_to_the_project_partner_(choose_all_that_apply)_?', 'cashless_transactions', 'distributed_team_effort_', 'board_of_directors', 'total_number_of_salaried_staff_supported', 'minimum_salary_(monthly)_of_a_teacher_or_any_other_salaried_staff_directly_involved_with_education_with_the_project_', 'maximum_salary_(monthly)_of_a_teacher_or_a_salaried_staff_directly_involved_with_education_with_the_project_', 'total_number_of_salaried_staff_of_the_project_partner', 'number_of_students_supported', 'number_of_students_impacted', 'number_of_girls_supported', 'number_of_boys_supported', 'number_of_special_needs_students_supported', 'average_monthly_household_income_of_students_supported_by_afe_(in_inr)', 'what_is_the_project_type?_', 'total_number_of_students_in_the_school.', 'average_number_of_years_the_above_set_of_students_were_supported_by_afe.', 'highest_level_of_education_provided_in_the_school', 'drop_out_rate_amongst_students.', 'continued_access_to_education.', 'total_number_of_teachers_in_the_school', 'highest_level_of_educational_attainment_of_the_teachers.', 'number_of_teachers_supported', 'average_monthly_salary_of_the_teachers_in_inr_', 'what_percent_of_monthly_teacher_salaries_is_supported_by_afe_funds', 'highest_level_of_educational_attainment_of_the_teachers..1', 'number_of_parateachers_supported', 'average_monthly_salary_of_the_parateachers_in_inr', 'what_percent_of_monthly_parateacher_salaries_supported_is_by_afe_funds', 'highest_level_of_educational_attainment_of_the_parateachers.', 'number_of_teachers_trained_per_year', 'highest_level_of_educational_attainment_of_the_teachers..2', 'number_of_training_hours_per_teachers__per_year__in_the_program', 'what_kind_of_teacher_training_is_provided?', 'number_of_days_of_training_per_student_per_year_', 'average_monthly_cost_of_training_per_student_in_inr', 'average_monthly_revenue_generated_from_the_sale_of_the_vocational_training_products_by_the_project_partner', 'what_%_of_the_vocational_trainees_eventually_get_gainfully_employed_after_completing_the_program?_', 'what_kind_of_vocational_training_is_offered_at_the_project?', 'number_of_hours_per_student_per_year', 'average_student/teacher_ratio_in_the_program', 'after-school_cost_per_student_per_month_in_inr', 'what_supplemental_facilities_are_provided_by_afe_funds?', 'number_of_days_per_year_the_residential_facility_was_provided', 'number_of_students_per_room', 'average_size_of_room_(in_sq._ft)', 'student_to_toilet_ratio', 'student_to_staff_ratio', 'number_of_meals_per_student_per_day', 'describe_meals_provided_to_students', 'average_monthly_cost_per_student_in_inr', 'number_of_meals_per_student_per_day.1', 'number_of_days_meals_provided_per_year', 'age_range_of_students', 'are_assessments_for_deficiencies_and_malnutrition_being_done?', 'average_monthly_cost_of_meal_in_inr', 'please_describe_meals_provided', 'type_of_material_(if_applicable)', 'average_cost_of_unit_of_material_in_inr', 'number_of_units_supported_by_afe', 'type_of_infrastructure_(if_applicable)', 'cost_of_infrastructure_unit_in_inr', 'number_of_units_supported_by_afe.1', 'describe_the_topic_of_awareness', 'number_of_days_spent_campaigning_per_year', 'number_of_unique_beneficiaries_targeted_per_year', 'number_of_staff_involved', 'cost_of_campaign_per_day_in_inr', 'percentage_of_awareness_campaigns_budget_supported_by_afe', 'how_does_the_project_partner_gauge_the_success_of_the_awareness_programs?_please_provide_measurable_goals__and_outcomes_for_impact.', 'please_describe_the_type_of_special_need', 'please_describe_the_type_of_service_provided_for_the_children', 'do_they_have_regular_assessments_and_medical_checkup_for_the_beneficiaries_and_monitor_progress_?', 'number_of_days_service_is_provided_per_year', 'student_to_care-giver/teacher_ratio_(if_applicable)', 'cost_of_service_per_student_per_day_(in_inr)', 'please_describe_the_type_of_activity', 'number_of_beneficiaries_per_year', 'number_of_indirect_beneficiaries_(students)', 'number_of_days_activity_was_conducted_per_year', 'cost_per_month_per_beneficiary_(in_inr)', 'percentage_of_capacity_building_budget_supported_by_afe', 'describe_the_community_service_being_provided_', 'number_of_unique_beneficiaries_targeted_per_year.1', 'number_of_days_program_is_conducted_per_year_', 'average_monthly_cost_per_beneficiary_in_inr', 'average_monthly_cost_of_staff_providing_the_service_in_inr', 'percentage_of_community_services_budget_supported_by_afe', 'describe_the_activity_to_be_conducted_by_the_fellow', 'number_of_fellowships_per_year', 'cost_per_month_per_fellow_in_inr', 'type_of_studies', 'total_budget_for_studies', 'type_of_studies.1', 'total_budget_for_studies_per_year', 'type_of_support_provided', 'total_budget_for_studies_per_year.1', 'percentage_of_budget_supported_by_afe', 'do_you_have_any_feedback_on_this_form_for_the_central_projects_team_?_we_thank_you_for_your_time_and_effort_in_completing_this_form.', 'are_you_done_filling_details_for_all_relevant_project_types?__(if_no__please_click_on_no__and_this_will_take_you_to_the_relevant_section)', 'rte_compliance', 'what_are_the_issues_being_faced_by_the_project_partner_to_get_towards_rte_compliance_?', 'date_of_submission_of_this_form']
+      '''
+
+import pandas as pd
+from utils.helpers import get_ratio, localeValue
+
+
+def get_KPI_summary(df: pd.DataFrame):
+    '''
+    children_supported : Total number of children supported
+    teachers/staff_supported : Total number of teachers/staff supported
+    projects_supported : Total number of projects supported
+    boys_girls_students_ratio : Ratio of boys to girls
+    avg_annual_salary_teachers : Average annual salary of teachers/Staff
+    funds_distributed : Total funds distributed
+    avg_annual_family_income : Average annual family income
+    paid_asha_employees : Total number of paid ASHA employees
+    asha_volunteer_hours : Total number of ASHA volunteer hours
+    '''
+    
+    kpis = {
+        'children_supported' : {
+            'title': 'Children Supported 🧒🏽',
+            'description': 'Total number of children supported',
+            'value' : df['number_of_students_supported'].sum(),
+            'formatted_value' : localeValue(df['number_of_students_supported'].sum())
+        },
+        'teachers/staff_supported' : {
+            'title': 'Teachers/Staff Supported 🧑🏼‍🏫',
+            'description': 'Total number of teachers/staff supported',
+            'value' : df['number_of_teachers_supported'].sum() + df['number_of_staff_involved'].sum(),
+            'formatted_value' : localeValue(df['number_of_teachers_supported'].sum() + df['number_of_staff_involved'].sum())
+            
+        },
+        'projects_supported' : {
+            'title': 'Projects Supported 🏫',
+            'description': 'Total number of projects supported',
+            'value' : df['project_url'].count(),
+            'formatted_value' : localeValue(df['project_url'].count())
+        },
+        'boys_girls_students_ratio' : {
+            'title': 'Boy:Girl\nStudent Ratio',
+            'description': 'Ratio of boys to girls',
+            'value' : get_ratio(df['number_of_boys_supported'].sum(), df['number_of_girls_supported'].sum()),
+            'formatted_value' : get_ratio(df['number_of_boys_supported'].sum(), df['number_of_girls_supported'].sum()),
+        },
+        'avg_annual_salary_teachers' : {
+            'title': 'Avg. Annual\nSalary for\nTeachers/Staff',
+            'description': 'Average annual salary of teachers/Staff',
+            'value' : df['average_monthly_salary_of_the_teachers_in_inr'].mean(),
+            'formatted_value' : '₹ '+localeValue(df['average_monthly_salary_of_the_teachers_in_inr'].mean())
+        },
+        # 'funds_distributed': {
+        #     'title': 'Funds Distributed',
+        #     'description': 'Total funds distributed',
+        #     'value' : df['total_funds_distributed'].sum(),
+        #     'formatted_value' : '₹ '+localeValue(df['total_funds_distributed'].sum())
+        # }
+        'avg_annual_family_income' : {
+            'title': 'Avg. Annual\nFamily Income',
+            'description': 'Average annual family income',
+            'value' : df['average_monthly_household_income_of_students_supported_by_afe_(in_inr)'].mean(),
+            'formatted_value' : '₹ '+localeValue(df['average_monthly_household_income_of_students_supported_by_afe_(in_inr)'].mean())
+        },
+        'paid_asha_employees': {
+            'title': 'Paid ASHA\nEmployees',
+            'description': 'Total number of paid ASHA employees',
+            'value' : 0,
+            'formatted_value' : 0
+        }
+        
+    }
+    return kpis
+    
+    
+def get_project_type_breakdown(df: pd.DataFrame):
+    
+    project_breakdown = df['what_is_the_project_type?'].value_counts().to_dict()
+    
+    data = []
+    other = 0
+    for key, value in project_breakdown.items():
+        if(value>3):
+            data.append({
+                'name' : key,
+                'value' : value
+            })
+        else:
+            other += value
+    for item in data:
+        if item['name'] == 'Other':
+            item['value'] += other
+            break
+    else:
+        data.append({
+            'name' : 'Other',
+            'value' : other
+        })
+        
+    return data
+
+def get_chapter_breakdown(df: pd.DataFrame):
+    
+    project_breakdown = df['chapter_affiliation'].value_counts().to_dict()
+    
+    data = []
+    other = 0
+    for key, value in project_breakdown.items():
+        if(value>1):
+            data.append({
+                'name' : key,
+                'value' : value
+            })
+        else:
+            other += value
+    
+    updated_data = {}
+    for item in data:
+        if get_common_name(item['name']) in updated_data:
+            updated_data[get_common_name(item['name'])] += item['value']
+        else:
+            updated_data[get_common_name(item['name'])] = item['value']
+
+    updated_data['Other'] = other
+    data = []
+    for key, value in updated_data.items():
+       
+       data.append({
+            'name' : key,
+            'value' : value
+        })
+    
+    
+     
+
+    return data
+
+
+def get_common_name(name):
+    
+    city_dic = {
+    "Seattle": "Seattle",
+    "Silicon Valley": "Silicon Valley",
+    "Chennai": "Chennai",
+    "Austin": "Austin",
+    "San Diego": "San Diego",
+    "Boston": "Boston",
+    "Bangalore": "Bangalore",
+    "Atlanta": "Atlanta",
+    "Boston/MIT": "Boston",
+    "Kansas City": "Kansas City",
+    "Cornell": "Cornell",
+    "Purdue": "Purdue",
+    "UIUC": "UIUC",
+    "St.Louis": "St Louis",
+    "San Francisco": "San Francisco",
+    "Berkeley": "Berkeley",
+    "Minnesota": "Minnesota",
+    "Chicago": "Chicago",
+    "Princeton": "Princeton",
+    "St. Louis": "St Louis",
+    "Danbury": "Danbury",
+    "Colorado": "Colorado",
+    "Athens": "Athens",
+    "Dallas": "Dallas",
+    "Berkeley ": "Berkeley",
+    "Zurich ": "Zurich",
+    "UFlorida": "UFlorida",
+    "Detroit": "Detroit",
+    "Bangalore/Central": "Bangalore",
+    "Madison": "Madison",
+    "Asha St. Louis": "St Louis",
+    "Detroit ": "Detroit",
+    "Yale": "Yale",
+    "UFlorida and Seattle": "UFlorida and Seattle",
+    "Sillicon Valley": "Silicon Valley",
+    "Stamford": "Stanford",
+    "Mumbai": "Mumbai",
+    "Asha Boston/MIT": "Boston",
+    "SV": "Silicon Valley",
+    "Asha Silicon Valley": "Silicon Valley",
+    "Stanford": "Stanford",
+    "Asha Cornell": "Asha Cornell",
+    "Frankfurt": "Frankfurt",
+    "Other": "Other"
+    }
+    
+    if name in city_dic:
+        return city_dic[name]
+    else:
+        return name
